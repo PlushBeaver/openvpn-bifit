788db96d79a35029032277b49d7ad5edd0f12df9
Dmitry Kozlyuk <dmitry.kozliuk@gmail.com>
2021-01-31 02:07:50 +0300

    ошибки update-systemd-resolved не мешают выключению

--- down.sh
+++ down.sh
@@ -4,4 +4,4 @@ while read rule; do
     ip rule del $rule
 done < /usr/local/share/rules
 
-test -e /dev/log && /usr/local/bin/update-systemd-resolved "$@"
+test -e /dev/log && /usr/local/bin/update-systemd-resolved "$@" || true
